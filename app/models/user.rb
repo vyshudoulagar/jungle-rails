7948ae5c71f5b7e3314faa72db096e7dde31ef36
WishNavy <du.vyshu@gmail.com>
2024-07-17 19:22:16 -0700

    add tests for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :email, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+
+    def self.authenticate_with_credentials(email, password)
+        normalized_email = email.strip.downcase
+        user = User.find_by(email: normalized_email)
+        if user && user.authenticate(password)
+            user
+        else
+            nil
+        end
+    end
 end
